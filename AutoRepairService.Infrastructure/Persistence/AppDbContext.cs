75196a2965855c40f9f1f907d4933c7a390277b9
saba kiladze <kiladze.saba2007@gmail.com>
2026-09-05 21:54:59 +0400

    attention to Configurations

--- AutoRepairService.Infrastructure/Persistence/AppDbContext.cs
+++ AutoRepairService.Infrastructure/Persistence/AppDbContext.cs
@@ -1,12 +1,39 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using AutoRepairService.Domain.Entities;
+using Microsoft.EntityFrameworkCore;
 
-namespace AutoRepairService.Infrastructure.Persistence
+namespace AutoRepairService.Infrastructure.Persistence;
+
+public class AppDbContext : DbContext
 {
-    public class AppDbContext
+    public AppDbContext(DbContextOptions<AppDbContext> options)
+        : base(options)
     {
+    }
+
+    //AppDbContext ის მთავარი დანიშნულებაა რომ ცხრილების მონაცემები აქციოს სიად.
+
+    public DbSet<User> Users => Set<User>();
+    public DbSet<Role> Roles => Set<Role>();
+    public DbSet<UserRole> UserRoles => Set<UserRole>();
+    public DbSet<MechanicProfile> MechanicProfiles => Set<MechanicProfile>();
+    public DbSet<CustomerProfile> CustomerProfiles => Set<CustomerProfile>();
+    public DbSet<Profile> Profiles => Set<Profile>();
+    public DbSet<Vehicle> Vehicles => Set<Vehicle>();
+    public DbSet<Card> Cards => Set<Card>();
+    public DbSet<MechanicBankAccount> MechanicBankAccounts => Set<MechanicBankAccount>();
+    public DbSet<Service> Services => Set<Service>();
+    public DbSet<Part> Parts => Set<Part>();
+    public DbSet<ServicePart> ServiceParts => Set<ServicePart>();
+    public DbSet<PayMent> Payments => Set<PayMent>();
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        modelBuilder.ApplyConfigurationsFromAssembly(
+            typeof(AppDbContext).Assembly);
+
+        base.OnModelCreating(modelBuilder);
+
+        //"Infrastructure assembly-ში მოძებნე ყველა IEntityTypeConfiguration<T> და გამოიყენე."
+        //    აღარაა საჭირო new ... შექმნა მათი გამოყენებისთვის
     }
 }
--- AutoRepairService.WebApi/Program.cs
+++ AutoRepairService.WebApi/Program.cs
@@ -1,3 +1,6 @@
+﻿
+using AutoRepairService.Infrastructure.Persistence;
+using Microsoft.EntityFrameworkCore;
 
 namespace AutoRepairService.WebApi
 {
@@ -8,6 +11,15 @@ namespace AutoRepairService.WebApi
             var builder = WebApplication.CreateBuilder(args);
 
             // Add services to the container.
+
+
+            //პოულობს მისამართს: appsettings.json-იდან ამოაქვს მონაცემთა ბაზის მისამართი (DefaultConnection).
+            //ირჩევს ბაზის ტიპს: პროგრამას ეუბნება, რომ გამოიყენოს Microsoft SQL Server-ი.
+            //ხდის ხელმისაწვდომს: ბაზის მართვის ხელსაწყოს (AppDbContext) ამზადებს მთელ პროგრამაში გამოსაყენებლად. პროგრამა ავტომატურად მართავს AppDbContext-ის შექმნას და გადაცემას იქ, სადაც ის გჭირდებათ (ამას .NET-ში Dependency Injection ჰქვია).
+            builder.Services.AddDbContext<AppDbContext>(options =>
+    options.UseSqlServer(
+        builder.Configuration.GetConnectionString("DefaultConnection")));
+
 
             builder.Services.AddControllers();
             // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
