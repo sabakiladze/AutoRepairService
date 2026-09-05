5caf76edb2eef3a42de105747870bf52fc1c9a15
sabakiladze <kiladze.saba2007@gmail.com>
2026-09-05 23:13:51 +0400

    i have to do authentification, includin jwt and email verification

---
+++ AutoRepairService.Application/ServiceInterfaces/IAuthentification.cs
@@ -0,0 +1,32 @@
+﻿using AutoRepairService.Domain.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AutoRepairService.Application.ServiceInterfaces
+{
+    public interface IAuthentification
+    {
+
+
+//        User რეგისტრირდება
+//       ↓
+//ვამოწმებთ Email-ს
+//       ↓
+//Password → Hash
+//       ↓
+//User იქმნება
+//       ↓
+//Verification Token იქმნება
+//       ↓
+//SMTP-ით იგზავნება Email
+//       ↓
+//User აჭერს Verify Email-ს
+//       ↓
+//Email დადასტურებულია
+//       ↓
+//შეუძლია Login
+    }
+}
